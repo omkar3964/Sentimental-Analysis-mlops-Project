5c9405728beac57329af6fc817f427b04d11740f
omkar3964 <omalbhare@gmail.com>
2025-09-27 08:28:53 +0530

    project structure ready.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project applies Machine Learning and MLOps practices to build, deploy, and monitor a sentiment analysis system. The goal is to classify text (such as customer reviews, tweets, or feedback) into categories like positive, negative, or neutral.',
+    author='omkar',
+    license='MIT',
+)
